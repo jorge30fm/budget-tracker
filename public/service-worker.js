f301dda0fd135d567813e36574b13d2721873d26
Jorge Felix <jorge30fm@gmail.com>
2022-08-22 12:02:19 -0400

    fix errors in manifest, link to index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,7 +41,11 @@ self.addEventListener("activate", function (event) {
 self.addEventListener("fetch", function (event) {
 	event.respondWith(
 		caches.match(event.request).then(function (request) {
-			return request || fetch(event.request);
+			if (request) {
+				return request;
+			} else {
+				return fetch(event.request);
+			}
 		})
 	);
 });
